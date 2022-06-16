04fe324ea464db2094f43354ccfab80e4ea4d8ec
st <pomalejko.vova@gmail.com>
2022-06-16 21:54:23 +0300

    Add ArraySearcher class with searcher Task02

---
+++ src/by/itstep/Pomaleiko/javalessons/lesson18/controller/Task02.java
@@ -0,0 +1,48 @@
+package by.itstep.Pomaleiko.javalessons.lesson18.controller;
+
+import by.itstep.Pomaleiko.javalessons.lesson18.model.logic.ArraySearcher;
+import by.itstep.Pomaleiko.javalessons.lesson18.model.logic.ArrayWorker;
+import by.itstep.Pomaleiko.javalessons.lesson18.util.ArrayInitializer;
+import by.itstep.Pomaleiko.javalessons.lesson18.util.Convertor;
+import by.itstep.Pomaleiko.javalessons.lesson18.view.Printer;
+
+import java.util.Scanner;
+
+public class Task02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size;
+        do {
+            Printer.print("Input size for array: ");
+            size = scanner.nextInt();
+        } while (size <= 0);
+
+        int[] array = new int[size];
+
+//        ArrayInitializer.userInit(array);
+        ArrayInitializer.randomInit(array);
+
+        Printer.print(Convertor.toString(array));
+
+        // logic
+
+        int value = 7;
+        boolean result = ArraySearcher.check(array, value);
+
+        String msg = result ? "Element exist." : "Element was not found.";
+        Printer.print("\n" + msg);
+
+//        int s = ArrayWorker.sum(array);
+//        int max = ArrayWorker.max(array);
+//        int min = ArrayWorker.min(array);
+//        double avg = ArrayWorker.avg(array);
+
+//        String msg = String.format("\nSum of array elements = %d.\n " +
+//                "Max element = %d, Min element = %d.\n" +
+//                "Average sum = %.2f", s, max, min, avg);
+
+        Printer.print(msg);
+        // output result
+
+    }
+}
